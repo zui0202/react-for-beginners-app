8e7454fc0d3696e900774be5a5bce4d2c90ce2ed
zui0202 <zui0202@naver.com>
2022-02-28 11:50:56 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App(){
-  return null;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/movie" element={<Detail />}></Route>
+    </Routes>
+  </Router>;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import {Link } from "react-router-dom";
 
 function Movie({medium_cover_image, title, summary, genres}){
     return (
         <div>
-            <h2>{title}</h2>
+            <h2><Link to="/movie">{title}</Link></h2>
             <img src={medium_cover_image} alt={title} />
             <p>{summary}</p>
             <ul>
@@ -19,7 +20,7 @@ Movie.propTypes = {
     medium_cover_image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string),
 };
 
 
