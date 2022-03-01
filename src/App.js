5f79831f59d656fd41fe3567ab68714e1969beb3
zui0202 <zui0202@naver.com>
2022-03-01 15:31:04 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Detail from "./routes/Detail";
 function App(){
   return <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route element={<Home />}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/movie/:id" element={<Detail />}></Route>
     </Routes>
   </Router>;
